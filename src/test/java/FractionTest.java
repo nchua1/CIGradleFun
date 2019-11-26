54412c216dd142db5b1c692f5f2926186f5304b4
Nick Chua <nickchua@cjca-23915d.gonzaga.edu>
2019-11-26 14:54:38 -0800

    force to fail

--- src/test/java/FractionTest.java
+++ src/test/java/FractionTest.java
@@ -9,7 +9,7 @@ public class FractionTest {
     public void ReduceTest() {
         Fraction f1 = new Fraction(2, 4);
         f1 = f1.reduce(f1);
-        assertEquals(f1.getNumerator(), 1);
+        assertEquals(f1.getNumerator(), 2);
         assertEquals(f1.getDenominator(), 2);
     }
 
